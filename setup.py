efd1cab82596ef6907c9052289748025b6b188b7
Felice Ho <codeLovingYogi@users.noreply.github.com>
2019-09-18 15:52:39 -0400

    Run black on setup.py

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ from setuptools import setup, find_packages
 from hambot import VERSION
 
 requires = [
-        'pyyaml==3.13',
-        'slackclient==1.3.0',
-        'redis==2.10.6',
-        'json2html==1.0.0',
-        'pytest==4.0.1'
+    "pyyaml==3.13",
+    "slackclient==1.3.0",
+    "redis==2.10.6",
+    "json2html==1.0.0",
+    "pytest==4.0.1",
 ]
 
 setup(
@@ -23,17 +23,17 @@ setup(
     version=VERSION,
     author="Equinox",
     description="",
-    long_description=open('README.rst').read(),
+    long_description=open("README.rst").read(),
     url="https://github.com/equinoxfitness/hambot",
     scripts=[],
     license="TBD",
     packages=find_packages(exclude=["tests*"]),
     install_requires=requires,
     classifiers=[
-        'Development Status :: 1 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-    ]
+        "Development Status :: 1 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+    ],
 )
