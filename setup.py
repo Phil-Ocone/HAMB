2025957e9d39840857e9941f75fb2e03b90e9e55
Felice Ho <codeLovingYogi@users.noreply.github.com>
2019-09-18 15:51:49 -0400

    Clean up setup.py file

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 
 """
-distutils/setuptools install script.
+setuptools install script.
 """
 import os
 import re
 
 from setuptools import setup, find_packages
 
-
-ROOT = os.path.dirname(__file__)
-VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.]+)['"]''')
+from hambot import VERSION
 
 requires = [
         'pyyaml==3.13',
@@ -20,16 +18,13 @@ requires = [
         'pytest==4.0.1'
 ]
 
-def get_version():
-    return "1.0.0"
-
 setup(
     name="hambot",
-    version=get_version(),
+    version=VERSION,
     author="Equinox",
     description="",
     long_description=open('README.rst').read(),
-    url="https://bitbucket.org/equinoxfitness/hambot",
+    url="https://github.com/equinoxfitness/hambot",
     scripts=[],
     license="TBD",
     packages=find_packages(exclude=["tests*"]),
@@ -40,11 +35,5 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
-    ],
-    dependency_links=[
-        "git+https://bitbucket.org/equinoxfitness/datacoco3.core.git#egg=cocore-1.0.0",
-        "git+https://bitbucket.org/equinoxfitness/datacoco3.cloud.git#egg=cocloud-1.0.0",
-        "git+https://bitbucket.org/equinoxfitness/datacoco3.utils.git@feature/python#egg=coutils-1.0.0",
-        "git+https://bitbucket.org/equinoxfitness/datacoco3.db.git@DATPLAT-391/replace-pymssql-to-pytds#egg=codb-1.0.0"
     ]
 )
