0746488db515319b48f6c685882eb13cd15ecf6a
Felice Ho <codeLovingYogi@users.noreply.github.com>
2019-10-09 16:07:00 -0400

    Run black on setup.py

--- setup.py
+++ setup.py
@@ -3,21 +3,16 @@
 """
 setuptools install script.
 """
-import os
-import re
-
 from setuptools import setup, find_packages
 
-ROOT = os.path.dirname(__file__)
-VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.]+)['"]''')
+from hambot import VERSION
 
 requires = [
-        'datacoco==0.1'
-        'pyyaml==3.13',
-        'slackclient==1.3.0',
-        'redis==2.10.6',
-        'json2html==1.0.0',
-        'pytest==4.0.1'
+    "datacoco==0.1" "pyyaml==3.13",
+    "slackclient==1.3.0",
+    "redis==2.10.6",
+    "json2html==1.0.0",
+    "pytest==4.0.1",
 ]
 
 setup(
@@ -32,10 +27,10 @@ setup(
     packages=find_packages(exclude=["tests*"]),
     install_requires=requires,
     classifiers=[
-        'Development Status :: 1 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-    ]
+        "Development Status :: 1 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+    ],
 )
